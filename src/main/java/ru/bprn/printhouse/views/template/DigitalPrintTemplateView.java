257eb3497b65133e8848ba3fd6d5272ae6d8a1ff
init6 <work@bprn.ru>
2024-06-13 08:43:52 +0700

    add some corrections in class DigitalPrintTemplateView.java

--- src/main/java/ru/bprn/printhouse/views/template/DigitalPrintTemplateView.java
+++ src/main/java/ru/bprn/printhouse/views/template/DigitalPrintTemplateView.java
@@ -3,6 +3,7 @@ package ru.bprn.printhouse.views.template;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.contextmenu.SubMenu;
 import com.vaadin.flow.component.grid.Grid;
@@ -50,25 +51,64 @@ public class DigitalPrintTemplateView extends VerticalLayout {
         MenuItem item = menuBar.addItem(new Icon(VaadinIcon.PLUS));
         SubMenu subMenu = item.getSubMenu();
         subMenu.addItem("Цифровая печать", menuItemClickEvent -> tabSheet.add(createTab("Цифровая печать"), new VerticalLayout()));
-        subMenu.addItem("Резка", menuItemClickEvent -> tabSheet.add("Резка", new VerticalLayout()));
-        subMenu.addItem("Верстка", menuItemClickEvent -> tabSheet.add("Верстка", new VerticalLayout()));
+        subMenu.addItem("Резка", menuItemClickEvent -> tabSheet.add(createTab("Резка"), new VerticalLayout()));
+        subMenu.addItem("Верстка", menuItemClickEvent -> tabSheet.add(createTab("Верстка"), new VerticalLayout()));
         tabSheet.setSuffixComponent(menuBar);
 
         add(grid,tabSheet);
     }
 
     private Tab createTab (String str){
-        var tab = new Tab(str);
+        var tab = new Tab();
         var la = new HorizontalLayout();
+        var vla = new VerticalLayout();
+        vla.setAlignItems(Alignment.CENTER);
+        vla.add(new Span(str));
+
         var leftBtn = new Button(VaadinIcon.CHEVRON_CIRCLE_LEFT_O.create());
+        leftBtn.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        leftBtn.addClickListener(e->{
+            Tabs tabs = (Tabs) tabSheet.getChildren().filter(Tabs.class::isInstance).findFirst().get();
+            int i =  tabSheet.getIndexOf(tab);
+            tabs.setSelectedIndex(i);
+            tab.setSelected(true);
+            if (i>0) {
+                Tab twoTab = tabSheet.getTabAt(i);
+                tabs.setSelectedIndex(i-1);
+                tabs.addTabAtIndex(i-1,twoTab);
+                tabSheet.setSelectedTab(tab);
+            }
+        });
+
+        var dialog = new ConfirmDialog("Вы хотите удалить "+tab.getLabel(), "Все изменения тоже удалятся...",
+                "Да",confirmEvent -> tabSheet.remove(tab), "Нет", cancelEvent -> cancelEvent.getSource().close());
 
         var closeBtn = new Button(VaadinIcon.CLOSE_CIRCLE_O.create());
-        closeBtn.addClickListener(e->tabSheet.remove(tab));
+        closeBtn.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        closeBtn.addClickListener(e->{
+            tabSheet.setSelectedTab(tab);
+            dialog.open();
+        });
 
         var rightBtn = new Button(VaadinIcon.CHEVRON_CIRCLE_RIGHT_O.create());
+        rightBtn.addThemeVariants(ButtonVariant.LUMO_SMALL);
+        rightBtn.addClickListener(e->{
+            Tabs tabs = (Tabs) tabSheet.getChildren().filter(Tabs.class::isInstance).findFirst().get();
+            int j = tabs.getComponentCount();
+            int i =  tabSheet.getIndexOf(tab);
+            tabs.setSelectedIndex(i);
+            tab.setSelected(true);
+            if (i+1<j) {
+                Tab twoTab = tabSheet.getTabAt(i+1);
+                tabs.setSelectedIndex(i);
+                tabs.addTabAtIndex(i,twoTab);
+                tabSheet.setSelectedTab(tab);
+            }
+        });
 
         la.add(leftBtn, closeBtn, rightBtn);
-        tab.add(la);
+        vla.add(la);
+        tab.add(vla);
         return tab;
     }
 
